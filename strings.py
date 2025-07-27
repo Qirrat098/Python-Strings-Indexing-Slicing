b51944e25040826fcdab6fb121ad96820a1ff9e6
Qirrat098 <qirratazam098@gmail.com>
2025-07-27 16:03:26 +0500

    strings

---
+++ strings.py
@@ -0,0 +1,4 @@
+#Strings are sequence of characters
+#Strings are immutable, Strings are iterable, Strings are indexed, Strings are ordered
+#In python, strings are a sequence of unicode characters
+
