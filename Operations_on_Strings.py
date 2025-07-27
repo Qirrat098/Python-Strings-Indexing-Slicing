0585ce91240ac6f59c4d837524ab2c8069d28971
Qirrat098 <qirratazam098@gmail.com>
2025-07-27 16:49:10 +0500

    Operations_on_Strings

---
+++ Operations_on_Strings.py
@@ -0,0 +1,12 @@
+print("*"*20) #Multiple of 20
+
+#True/False Operations on Strings based on Ascii values
+print("Karachi" == "Karachi") #True
+print("Karachi" == "karachi") #False
+print("Karachi" != "Karachi") #False
+print("Karachi" != "karachi") #True
+print("Karachi" > "Karachi") #False
+print("Karachi" > "karachi") #False
+print("Karachi" < "Karachi") #False
+print("Karachi" < "karachi") #True 
+
