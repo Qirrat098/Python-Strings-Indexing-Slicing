4690aa2f8edb0cb9588f2380b38c4c239ceb0de5
Qirrat098 <qirratazam098@gmail.com>
2025-07-27 16:21:56 +0500

    positve indexing

---
+++ slicing.py
@@ -0,0 +1,10 @@
+#to extract a substring from a string
+
+s = "Hello, World!"
+print(s[0:5])  # Output: Hello
+print(s[7:12])  # Output: World
+print(s[2:])  # Output: llo, World!
+print(s[:5])  # Output: Hello
+print(s[:]) # Output: Hello, World!
+print(s[0:12:3]) # Output: Hlo
+
